cc0793a0611a0262e9fc0dd5c5696e63710e864e
sujithreddydharmareddy <dharmareddysujith15@gmail.com>
2023-06-24 12:19:26 -0500

    Updated PostMapping logic

--- src/main/java/org/example/Addition.java
+++ src/main/java/org/example/Addition.java
@@ -12,9 +12,46 @@ public class Addition {
     Calci calci;
 
     @PostMapping("/add")
-    public ResponseEntity<Long> addNumbers(@RequestBody TwoNumbers twoNumbers) {
+    public ResponseEntity<Result> addNumbers(@RequestBody TwoNumbers twoNumbers) {
         long result = calci.add(twoNumbers.getNum1(), twoNumbers.getNum2());
-        return ResponseEntity.ok(result);
+        Result result1 = new Result(twoNumbers.getNum1(), twoNumbers.getNum2(), result);
+        return ResponseEntity.ok(result1);
+    }
+
+    public static class Result {
+        private long num1;
+        private long num2;
+        private long sum;
+
+        public Result(long num1,long num2,long sum) {
+            this.num1=num1;
+            this.num2=num2;
+            this.sum = sum;
+        }
+
+        public long getSum() {
+            return sum;
+        }
+
+        public void setSum(long sum) {
+            this.sum = sum;
+        }
+
+        public long getNum1() {
+            return num1;
+        }
+
+        public long getNum2() {
+            return num2;
+        }
+
+        public void setNum1(long num1) {
+            this.num1 = num1;
+        }
+
+        public void setNum2(long num2) {
+            this.num2 = num2;
+        }
     }
 
 
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,12 +1,11 @@
 package org.example;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 import org.junit.Assert.*;
 import org.junit.jupiter.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -24,10 +23,20 @@ public class AppTest
     @Test
     public void AddTwoNumbers()
     {
-        long a=4,b=6;
         long expectedSum = 10;
         long actualSum = calci.add(4,6);
         assertEquals(expectedSum, actualSum);
 
     }
+
+    @Test
+    public void AddTwoNumbersFail()
+    {
+        long expectedSum = 11;
+        long actualSum = calci.add(4,6);
+        assertNotEquals(expectedSum, actualSum);
+
+    }
+
+
 }
